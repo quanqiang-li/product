cd14e023d638ec4aafbe504e317475758bd58263
aisino-liqq <liquanqiang@aisino.com>
2019-03-18 19:53:40 +0800

    简单的服务

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -4,9 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import dto.ProductDTO;
 
 /**
  * 登录
@@ -15,6 +14,7 @@ import dto.ProductDTO;
  *
  */
 @RestController
+@RequestMapping("product")
 public class ProductController {
 
 	@Value("${server.port}")
@@ -22,13 +22,9 @@ public class ProductController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@PostMapping("getProduct")
-	public ProductDTO getProduct(ProductDTO productDTO) {
-		logger.info("请求端口{}的产品{}", port, productDTO.toString());
-		// 1.从缓存取产品 [稳定数据,使用频繁,放在缓存合适]
-		ProductDTO vo = new ProductDTO();
-		vo.setId(productDTO.getId());
-		vo.setProductName("mate 20 PRO");
-		return vo;
+	public String getProduct(String productName) {
+		logger.info("请求端口{}的产品{}", port, productName);
+		return "请求端口" + port + "的产品" + productName;
 	}
 
 }
